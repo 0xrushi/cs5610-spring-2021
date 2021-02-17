c76f1028a473b337931973ee21877ae5c15f3713
Rushi <rushic24@gmail.com>
2021-02-17 00:26:50 -0500

    first client

---
+++ webdev/2021/spring/03/wbdv-sp21-01-rushi-client-react/src/components/course-editor.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// const CourseEditor = () => {
+//     return (
+//         <h1>Hello from CouresEditor</h1>
+//     )
+// }
+
+const CourseEditor = () => <h1>Hello from CouresEditor</h1>
+
+
+export default CourseEditor
