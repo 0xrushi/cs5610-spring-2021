2f8981c9fa21ffde379bc1d7f1d6c4d62b400cab
rushichaudhari <53929994+rushichaudhari@users.noreply.github.com>
2021-02-20 01:49:20 -0500

    react client base draft

--- webdev/2021/spring/03/wbdv-sp21-01-rushi-client-react/src/App.js
+++ webdev/2021/spring/03/wbdv-sp21-01-rushi-client-react/src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import CourseManager from "./components/course-manager";
 import CourseEditor from "./components/course-editor";
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 function App() {
   return (
-    <div className="container-fluid">
-        <CourseManager/>
-        <CourseEditor/>
-    </div>
+    <BrowserRouter>
+      <div className="container-fluid">
+          <CourseManager/>
+      </div>
+    </BrowserRouter>
   );
 }
 
